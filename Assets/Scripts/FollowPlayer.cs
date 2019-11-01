445b09f946edb18c5253ae7823fe16f425b8db18
Rasmus Ingelin <brain001@edu.xamk.fi>
2019-11-01 14:25:10 +0200

    AutoPeli

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public Vector3 offset;
+    public GameObject FollowedObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = FollowedObject.transform.position + offset;
+        transform.LookAt(FollowedObject.transform);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float Speed;
+    public float rotationSpeed;
+    public float Vertical;
+    public float Horizontal;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.rotationSpeed = 30f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.Vertical = Input.GetAxis("Verical");
+        this.Horizontal = Input.GetAxis("Horizontal");
+
+        this.transform.Translate(this.transform.forward * Speed * this.Vertical * Time.deltaTime);
+        this.transform.Rotate(new Vector3(0, 30 * this.Horizontal * Time.deltaTime, 0));
+    }
+}
+ 
